a018a09b6638d1824a208f8de36172daa4a586be
tawsifhye <tawsif.lucse@gmail.com>
2021-10-21 12:47:50 +0600

    Create dynamic api, api parameter, access params

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// const handler = (req, res) => {
+//   res.send("Hello from Node eeeeeeeeeeeh");
+// };
+
+// app.get("/", handler);
+
+const users = [
+  {
+    id: 0,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 1,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 2,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 3,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  },
+  {
+    id: 4,
+    name: "Chelsey Dietrich",
+    username: "Kamren",
+    email: "Lucio_Hettinger@annie.ca",
+  },
+];
+
+app.get("/users", (req, res) => {
+  res.send(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  //   console.log(req.params);
+  const user = users[id];
+  res.send(user);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
