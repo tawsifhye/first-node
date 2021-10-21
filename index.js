5c89f237492d5db8b8b29c2b906ce5d38644475c
tawsifhye <tawsif.lucse@gmail.com>
2021-10-21 16:04:25 +0600

    Use fetch to load data from server, middleware, handle cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+app.use(cors());
+const port = process.env.PORT || 5000;
 
 // const handler = (req, res) => {
 //   res.send("Hello from Node eeeeeeeeeeeh");
